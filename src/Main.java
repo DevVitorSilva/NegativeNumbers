516099c9fca916896f060f87ae0c5cea4190d0b8
DevVitorSilva <vitor.silva001080@gmail.com>
2024-01-24 08:52:25 -0300

    resolved problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Quantos números você vai digitar? ");
+        int inputNumber = sc.nextInt();
+        int[] arrayNumbers = new int[inputNumber];
+        for (int i = 0; i < arrayNumbers.length; i++){
+            System.out.print("Digite um número inteiro: ");
+            arrayNumbers[i] = sc.nextInt();
+        }
+        System.out.println("Números negativos:");
+        for (int i = 0; i < arrayNumbers.length; i++){
+            if(arrayNumbers[i] < 0){
+                System.out.println(arrayNumbers[i]);
+            }
+        }
+        sc.close();
     }
 }
